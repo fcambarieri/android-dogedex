e62c9c03a485783af3c708f999a44add55b689b3
Fernando Cambarieri <fernando.cambarieri@etermax.com>
2023-12-26 11:31:26 -0300

    adding viewmodal

--- app/src/main/java/com/hackaprende/dogedex/doglist/DogListActivity.kt
+++ app/src/main/java/com/hackaprende/dogedex/doglist/DogListActivity.kt
@@ -1,6 +1,7 @@
 package com.hackaprende.dogedex.doglist
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.hackaprende.dogedex.Dog
@@ -8,65 +9,27 @@ import com.hackaprende.dogedex.R
 import com.hackaprende.dogedex.databinding.ActivityDogListBinding
 
 class DogListActivity : AppCompatActivity() {
+
+    private val dogListViewModel : DogListViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dog_list)
+
         val binding = ActivityDogListBinding.inflate(layoutInflater)
-        val dogList = getFakeDogs()
+        setContentView(binding.root)
 
         val recycler = binding.dogRecycler
         recycler.layoutManager = LinearLayoutManager(this)
 
         val adapter = DogAdapter()
+
         recycler.adapter = adapter
 
-        adapter.submitList(dogList)
+        dogListViewModel.dogList.observe(this) {
+            adapter.submitList(it)
+        }
+
     }
 
-    private fun getFakeDogs(): MutableList<Dog> {
-        val dogList = mutableListOf<Dog>()
-        dogList.add(
-            Dog(
-                1, 1, "Chihuahua", "Toy", 5.4,
-                6.7, "", "12 - 15", "", 10.5,
-                12.3
-            )
-        )
-        dogList.add(
-            Dog(
-                2, 1, "Labrador", "Toy", 5.4,
-                6.7, "", "12 - 15", "", 10.5,
-                12.3
-            )
-        )
-        dogList.add(
-            Dog(
-                3, 1, "Retriever", "Toy", 5.4,
-                6.7, "", "12 - 15", "", 10.5,
-                12.3
-            )
-        )
-        dogList.add(
-            Dog(
-                4, 1, "San Bernardo", "Toy", 5.4,
-                6.7, "", "12 - 15", "", 10.5,
-                12.3
-            )
-        )
-        dogList.add(
-            Dog(
-                5, 1, "Husky", "Toy", 5.4,
-                6.7, "", "12 - 15", "", 10.5,
-                12.3
-            )
-        )
-        dogList.add(
-            Dog(
-                6, 1, "Xoloscuincle", "Toy", 5.4,
-                6.7, "", "12 - 15", "", 10.5,
-                12.3
-            )
-        )
-        return dogList
-    }
+
 }
